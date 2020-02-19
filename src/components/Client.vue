0fa06a705daa4d6f1afec749812179ce25f41ad0
Guilherme <guilhermeborgesti@gmail.com>
2020-02-18 21:19:15 -0300

    criacao do formulario e chamada do servico de caddastro do client

--- src/components/Client.vue
+++ src/components/Client.vue
@@ -1,15 +1,45 @@
 <template>
     <div>
-        <h2 v-for="result in res" v-bind:key="result"> {{ result }} </h2>
-        <input v-model="cep" @change="buscaCep">
-        <select @change="change" v-model="estado">
-          <option disabled >Selecione um estado</option>
-          <option v-for="estado in estados" :value="estado" :key="estado">{{estado.name}}</option>
-        </select>
-        <select  v-model="cidade">
-          <option disabled value="">Selecione uma cidade</option>
-          <option v-for="cidade in cidades" :key="cidade" :value="estado">{{cidade.name}}</option>
-        </select>
+      <label for="nome">Nome</label>
+      <input id="nome" v-model="name">
+      <br /><br />
+      <label for="cpf">Cpf</label>
+      <input id="cpf" v-model="cpf">
+      <h2 v-for="result in res" :key="result"> {{ result }} </h2>
+      <br /><br />
+      <label for="idade">Idade</label>
+      <input type="number" v-model="idade">
+      <br /> <br />
+      <label for="cep">Cep</label>
+      <input v-model="cep" @change="buscaCep">
+      <br ><br />
+      <label for="Pais">Pais</label>
+      <input v-model="pais">
+      <br ><br />
+      <label for="estado">Estado</label>
+      <select @change="change" v-model="estado">
+        <option disabled >Selecione um estado</option>
+        <option v-for="estado in estados" :value="estado" :key="estado">{{estado.name}}</option>
+      </select>
+      <br /><br />
+      <label for="cidade">Cidade</label>
+      <select  v-model="cidade">
+        <option disabled value="">Selecione uma cidade</option>
+        <option v-for="cidade in cidades" :key="cidade" :value="estado">{{cidade.name}}</option>
+      </select>
+      <br/><br/>
+      <label for="bairro">Bairro</label>
+      <input v-model="bairro">￼
+      <br /> <br />
+      <label for="rua">Rua</label>
+      <input v-model="rua">
+      <br /><br />
+      <label for="number">Número</label>
+      <input v-model="numero">
+      <br /><br />
+      <label for="number">Telefone</label>
+      <input v-model="telefone">
+      <button @click="salvar">Salvar</button>
     </div>
 </template>
 
@@ -25,7 +55,15 @@ export default{
       estados: [],
       estado: {},
       cidades: [],
-      cidade: {}
+      cidade: {},
+      numero: '',
+      telefone: '',
+      cpf: '',
+      nome: '',
+      pais: '',
+      bairro: '',
+      rua: '',
+      age: ''
     }
   },
   methods: {
@@ -41,6 +79,27 @@ export default{
       HTTP_SERVICE().get(this, this.urlServer + 'assistent/city/findByState/' + this.estado.id, (response) => {
         this.cidades = response
       })
+    },
+    salvar () {
+      let body = {
+        clientName: this.nome,
+        cpf: this.cpf,
+        telephone: this.telefone,
+        country: this.pais,
+        state: this.estado,
+        city: this.cidade,
+        district: this.bairro,
+        number: this.numero,
+        street: this.rua,
+        cep: this.zipCode,
+        age: this.idade
+      }
+      HTTP_SERVICE().post(this, this.urlServer + 'assistent/client/insertClient', (response) => {
+
+      }, (response) => {
+
+      }, body
+      )
     }
   },
   created () {
--- src/util/requisitions.js
+++ src/util/requisitions.js
@@ -1,11 +1,20 @@
 export const HTTP_SERVICE = () => {
   return {
-    get: (self, url, callbackSuccess, callbackFail) => {
+    get: (self, url, callbackSuccess, callbackFaill) => {
       self.$http({method: 'GET', url: url}).then((response) => {
         if (response.status === 200) { callbackSuccess(response.body) }
       },
       (response) => {
-        callbackFail(response.body)
+        callbackFaill(response.body)
+      })
+    },
+    post: (self, url, callbackSuccess, callbackFaill, body) => {
+      self.$http({method: 'POST', url: url, body: body}).then((response) => {
+        if (response.status === 200 || response.status === 201) {
+          callbackSuccess(response.body)
+        }
+      }, (response) => {
+        callbackFaill(response.body)
       })
     }
   }
