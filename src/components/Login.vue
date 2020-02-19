55e0ebdc4dac23be79f6a40843a7f60889d48bd7
Guilherme <guilhermeborgesti@gmail.com>
2020-02-19 20:20:50 -0300

    inclusao da tela de login

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <label for="user">Usuário</label>
+        <input v-model="user">
+        <br /><br />
+        <label for="password">Senha</label>
+        <input type="password" v-model="password"/>
+
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Index from '@/components/Index'
 import Client from '@/components/Client'
+import Login from '@/components/Login'
 
 const routes = [
   {
@@ -11,6 +12,11 @@ const routes = [
     path: '/client',
     name: Client,
     component: Client
+  },
+  {
+    path: '/login',
+    name: Login,
+    component: Login
   }
 ]
 export default routes
